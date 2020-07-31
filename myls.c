5550241b40419aca2b3b936e7f4c68ba6bdb7312
Kevin <kevin.chensu@gmail.com>
2020-07-31 10:52:48 -0700

    initial commit

---
+++ myls.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <grp.h>
+#include <pwd.h>
+#include <time.h>
+
+
+int main(int argc, char *argv[])
+{
+    
+}
