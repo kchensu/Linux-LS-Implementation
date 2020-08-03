c2b8332f62a12785c8b9b294e30fa46ad0a6ce0b
Kevin <kevin.chensu@gmail.com>
2020-08-02 20:47:25 -0700

    fixing bug

--- myls.c
+++ myls.c
@@ -8,8 +8,8 @@
 #include <time.h>
 #include <errno.h>
 #include <ctype.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char *getUserName(uid_t uid)
 {
@@ -50,156 +50,109 @@ int main(int argc, char *argv[])
 {
   DIR *dir;
   struct dirent *dp;
-  struct stat buf;
+  struct stat s_stats;
   char *file_name;
 
   // only ./UnixLs was entered, therefore ls is run on the cwd "."
   if (argc == 1)
   {
     dir = opendir(".");
-
     while ((dp = readdir(dir)) != NULL)
     {
       if (dp->d_name[0] != '.')
       {
         file_name = dp->d_name;
-        printf("%s\n", file_name);   
+        printf("%s\n", file_name);
+        
       }
     }
 
-    closedir(dir);
   }
+    closedir(dir);
 
-  // must check for [OPTION], [PATH], etc.
-  if (argc >= 2)
-  {
-    char *option_or_path = argv[1];
-    char *path = (char *)malloc(255 * sizeof(char));
-    char *fullpath = (char *)malloc(255 * sizeof(char));
-    char *filepath;
-    char *file_name;
-    char *link_name;
-    ssize_t r;
-
-    if (!strcmp(option_or_path, "ls"))
+    if (argc == 2)
     {
-      printf("ERROR: invalid formatting, this program runs by executing ./UnixLs [OPTION] [PATH].\n");
-      printf("EXAMPLE: ./UnixLs -il ..\n");
-      exit(1);
-    }
+        char *option_or_path = argv[1];
+        char *path = (char *)malloc(255 * sizeof(char));
+        char *fullpath = (char *)malloc(255 * sizeof(char));
+        char *filepath;
+        char *file_name;
+        char *link_name;
+        ssize_t r;
 
-    // checking that -i, -l, -il, or -li was used
-    if (option_or_path[0] == '-' && (option_or_path[1] == 'i') || option_or_path[1] == 'l')
-    {
-      // check for edgecases "./UnixLs -i -l [PATH]" or "./UnixLs -l -i [PATH]"
-      if (argc >= 3)
-      {
-        char *check_il_or_li_edgecase = argv[2];
-        if (!strcmp(check_il_or_li_edgecase, "-i") || !strcmp(check_il_or_li_edgecase, "-l"))
+        if (option_or_path[0] == '-' && (option_or_path[1] == 'i' || option_or_path[1] == 'l'))
         {
-          strncat(option_or_path, &check_il_or_li_edgecase[1], 1);
-        }
-      }
-
-      // [PATH] was not given, therefore do ls with [OPTION] on cwd
-      if (!strcmp(argv[argc - 1], option_or_path))
-      {
-        filepath = ".";
-      }
-      else
-      {
-        // copy [PATH] to filepath
-        strcpy(path, argv[argc - 1]);
-        filepath = path;
-      }
-    }
-    else
-    {
-      // no [OPTION] specified, just copy the path
-      strcpy(path, option_or_path);
-      filepath = path;
-    }
-
-    dir = opendir(filepath);
-
-    // illegal path; doesn't contain '/', '~', '.', '..'
-    if (dir == NULL)
-    {
-      printf("File path [%s] was not found!\n", filepath);
-      exit(1);
-    }
-    else
-    {
-      while ((dp = readdir(dir)) != NULL)
-      {
-        strcpy(fullpath, filepath);
-        strcat(fullpath, "/");
-        strcat(fullpath, dp->d_name);
-
-        file_name = dp->d_name;
-
-        // file exists
-        if (stat(fullpath, &buf) == 0)
-        {
-          if (file_name[0] != '.')
-          {
-            if (option_or_path[1] == 'i' && option_or_path[2] != 'l')
+            if (argc == 3)
             {
-              printInode(buf);
+                char *check = argv[2];
+                if (!strcmp(check, "-i") || strcmp(check, "-l"))
+                {
+                    strncat(option_or_path, &check[1], 1);
+                } 
             }
-
-            else if ((option_or_path[1] == 'i' && option_or_path[2] == 'l') || (option_or_path[1] == 'l' && option_or_path[2] == 'i'))
+            if (!strcmp(argv[argc - 1], option_or_path))
             {
-              printInode(buf);
-      
-            }
-
-            
-            printf("%s", file_name);
-            
-
-            // handle symbolic link
-            if (lstat(fullpath, &buf) == -1)
-            {
-              printf("\n");
+                filepath = ".";
             }
             else
             {
-              link_name = malloc(buf.st_size + 1);
-              r = readlink(fullpath, link_name, buf.st_size + 1);
-
-              if (r < 0 || r > buf.st_size)
-              {
-                printf("\n");
-              }
-              else
-              {
-                link_name[buf.st_size] = '\0';
-                printf(" -> %s\n", link_name);
-              }
-
-              free(link_name);
+                strcpy(path, argv[argc - 1]);
+                filepath = path;
             }
-          }
         }
         else
         {
-          printf("Stat failed: %s\n\n", strerror(errno));
+            strcpy(path, option_or_path);
+            filepath = path;
         }
-      }
+        dir = opendir(filepath);
+        while ((dp = readdir(dir)) != NULL)
+        {
+            strcpy(fullpath, filepath);
+            strcat(fullpath, "/");
+            strcat(fullpath, dp->d_name);
+
+            file_name = dp->d_name;
+            if (stat(fullpath, &s_stats) == 0)
+            {
+                if (file_name[0] != '.')
+                {
+                    if (option_or_path[1] == 'i' && option_or_path[2] != 'l')
+                    {
+                        printInode(s_stats);
+                    }
+                    else if (option_or_path[1] == 'l' && option_or_path[2] != 'i')
+                    {
+                        printInode(s_stats);
+                    }
+                    else if ((option_or_path[1] == 'i' && option_or_path[2] == 'l' )|| (option_or_path[1] == 'l' && option_or_path[2] == 'i'))
+                    {
+                        printInode(s_stats);
+                    }
+                    printf("%s\n", file_name);
+                }
+            }
+
+        }
+        free(link_name);
+        free(path);
+        free(fullpath);
+        closedir(dir);
+        
+
+       
+        
     }
 
-    free(path);
-    free(fullpath);
-    closedir(dir);
+
+
+    return 0;
+
+
+
+
+    
   }
 
-  return 0;
-}
-
-
-
-
-
-
-
+ 
+  
