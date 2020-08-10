4f8229fc6f9bcf7d1371f274b5b2eef8ea995b48
nathan <nsimms1333@gmail.com>
2020-08-09 17:13:23 -0700

    add arument sorting

--- myls.c
+++ myls.c
@@ -12,10 +12,7 @@
 #include <grp.h>
 #include <dirent.h>
 
-
 # define DATE_BUFFER 512
-#define _SVID_SOURCE
-
 
 typedef struct Options {
     int option_i;
@@ -43,9 +40,9 @@ void get_filename(char*, struct stat, Option *);
 void print_file(char*,Option*); 
 void print_directory(char*, Option*);
 
-extern char *optarg;
 extern int optind, opterr, optopt;
-
+static int count_opts = 0;
+static int non_opts = 0;
 void get_options(const int argc, char **argv, Option* opt) {
     int choice;
     struct stat s_stat;
@@ -55,19 +52,27 @@ void get_options(const int argc, char **argv, Option* opt) {
         switch(choice) {
             case 'i':  
                 opt->option_i = 1;
+                count_opts++;
                 break;
             case 'l':  
                 opt->option_l = 1;
+                count_opts++;
                 break;
             case 'R':  
                 opt->option_R = 1;
+                count_opts++;
                 break;
             default:
                 exit(1);  
             
         }
     }
-    
+    for (int i = optind; i < argc; i++)
+    {
+        // printf("NON ARGUMENTS %s\n", argv[i]);
+        non_opts++;
+        
+    }
 
 }
 
@@ -112,32 +117,33 @@ void get_permissions(struct stat path_stats) {
 void get_hardlink(struct stat s_stat)
 {
     int max_width = 2;
-    printf("%*ld", max_width, s_stat.st_nlink);
+    printf("%*ld ", max_width, s_stat.st_nlink);
 }
 void get_user_info(struct stat s_stat) 
 {   
     int max_width = 2;
-    printf("%*s", max_width, getpwuid(s_stat.st_uid)->pw_name);
+    printf("%*s ", max_width, getpwuid(s_stat.st_uid)->pw_name);
 }
 
 void get_group_info(struct stat s_stat) 
 {
-    int max_width = 4;
-    printf("%*s",max_width, getgrgid(s_stat.st_gid)->gr_name);
+    int max_width = 2;
+    printf("%*s ",max_width, getgrgid(s_stat.st_gid)->gr_name);
 }
 
-void get_file_size(struct stat s_stat) {
-    printf("%5ld  ", s_stat.st_size);
+void get_file_size(struct stat s_stat) 
+{   
+    printf("%5ld ", s_stat.st_size);
 }
 
 void get_date_time(struct stat s_stat) {
     struct tm *time;
     char *buffer = (char*)malloc(255);
-    int max_width = 20;
+    int max_width = 10;
     time_t time_s = s_stat.st_ctime;
     time = localtime(&time_s);
-    strftime(buffer, DATE_BUFFER ,"%b %d %Y %H:%M",time);
-    printf("%.*s", max_width, buffer);
+    strftime(buffer, DATE_BUFFER ,"%b %_d %Y %H:%M ",time);
+    printf("%*s", max_width, buffer);
     free(buffer);
 }
 int valid_link(char* path)
@@ -154,48 +160,65 @@ int valid_link(char* path)
 void get_filename(char* name,struct stat s_stat, Option *opt) {
     char *buffer = (char*)malloc(PATH_MAX);
     struct stat temp_stat;
-    int max_width = 100;
+    int max_width = 512; 
+    
 
-    // check if is afile
+    //check if is afile
     if (S_ISREG(s_stat.st_mode)) {
-        printf("%.*s ", max_width, name);
+        printf("%.*s\n", max_width, name);
         return;
     }
 
     // check if is a directory
-    if (S_ISDIR(s_stat.st_mode)) {
-        printf("%.*s ", max_width, name);
+    if (S_ISDIR(s_stat.st_mode)) 
+    {
+        printf("%.*s\n", max_width, name);
         return;
     }
-
-    // check if is a link, if is a link, then we will need to print out the file/dir its poiting to.
-    if (S_ISLNK(s_stat.st_mode)) 
+    // check if is a link
+    if (S_ISLNK(s_stat.st_mode))
     {
+        
         printf("%.*s", max_width, name);
-        readlink(name, buffer, PATH_MAX);
-        lstat(buffer, &temp_stat);
-        if (S_ISLNK(s_stat.st_mode) && opt->option_l)
-     
-        printf (" -> ");
+    }
+    
+    // buffer contains the link of a filename
+    
+    readlink(name, buffer, PATH_MAX);
+    lstat(buffer, &temp_stat);
+
+    //if is a link, then we will need to print out the file/dir its poiting to.
+    if (S_ISLNK(s_stat.st_mode) && opt->option_l) 
+    {
+        printf ("-> ");
         // printf("buffer : %s\n", buffer);
         // printf("name : %s\n", name);
         // check if is a file
+        
         if (S_ISREG(temp_stat.st_mode))
         {
+            
             if (temp_stat.st_mode & S_IXOTH) 
             {
-                printf("%.*s\n", max_width, name); 
-                return;
+                printf("%.*s", max_width, buffer); 
+               
             }
-            printf("%.*s\n", max_width, name);
+            
+            printf("%.*s", max_width, buffer);
+        }
+        if (S_ISLNK(temp_stat.st_mode))
+        {
+            printf("%.*s", max_width, buffer);
+          
         }
 
-        // check if is a directory
+        //check if is a directory
         if (S_ISDIR(temp_stat.st_mode)) 
         {
-            printf("%.*s\n", max_width,buffer);
+            printf("%.*s", max_width,buffer);
         }
     }
+    printf("\n");
     free(buffer);
 }
 
@@ -291,6 +314,33 @@ void print_directory(char *path, Option *option) {
         printf("error opening directory");
         exit(1);
     }
+    // while((dp = readdir(cwd)) != NULL) {     
+    //     // if (S_ISDIR(cur_stat.st_mode)) {
+    //     //     // printf("Debug: here\n");
+    //     //     printf("Debug: %s is a directory\n", dp->d_name);
+    //     // }   
+    //     if (dp->d_name[0] == '.') { // if hidden
+
+    //             continue; // skip hidden item if not set 
+    //     }
+    //     strcpy(buffer, dp->d_name);
+    //     if (lstat(buffer, &cur_stat) == -1) { // this happens when files of sub directories are arguments
+    //         // handle the case by appending file to cwd 
+    //         strcpy(buffer, path);
+    //         strcat(buffer, "/");
+    //         strcat(buffer, dp->d_name);
+    //         lstat(buffer, &cur_stat);
+    //     }
+    //     if (option->option_i) {
+    //         get_ino(cur_stat);
+    //     }
+    //     if (option->option_l) {
+
+    //         get_permissions(cur_stat);
+            
+    //     }
+    //     get_filename(dp->d_name, cur_stat, option);
+    // }
 
     struct dirent **namelist;
     int n;
@@ -299,24 +349,29 @@ void print_directory(char *path, Option *option) {
         perror("scandir");
         exit(EXIT_FAILURE);
     }
+    // printf("%s\n", path);
     // https://stackoverflow.com/questions/4989431/how-to-use-s-isreg-and-s-isdir-posix-macros
-    for(int i =0; i < n; i++) {  
+    for(int i =0; i < n; i++) 
+    {  
+        // get_permissions(cur_stat);
         dp = namelist[i];   
         if (dp->d_name[0] == '.') { 
             continue;     
         }
+        
         //https://stackoverflow.com/questions/19663042/stat-function-call
-        // stat() returns information about a file pointed to by pathname
+        // lstat() returns information about a file pointed to by pathname
         strcpy(buffer, path);
         strcat(buffer, "/");
         strcat(buffer, dp->d_name);  
-        int result = stat(buffer, &cur_stat);
+        int result = lstat(buffer, &cur_stat);
         if (result == -1)
         {
             printf("error %d\n", errno);
             exit(1);
         }
         if (option->option_i) {
+            
             get_ino(cur_stat);
         }
         if (option->option_l) {
@@ -332,6 +387,7 @@ void print_directory(char *path, Option *option) {
     printf("\n");
     free(dp);
     free(buffer);
+    free(namelist);
     closedir(cwd);
 }
 
@@ -339,7 +395,7 @@ void print_directory(char *path, Option *option) {
 void print_file(char* file_path, Option* option) { 
     struct stat s_stat;
     int file;
-    
+
     if (option->option_i) {
         get_ino(s_stat);
      
@@ -347,12 +403,14 @@ void print_file(char* file_path, Option* option) {
     if (option->option_l) 
     {
         get_permissions(s_stat);
+        get_hardlink(s_stat);
         get_user_info(s_stat);
         get_group_info(s_stat);
         get_file_size(s_stat);
         get_date_time(s_stat);
     }
     get_filename(file_path, s_stat, option);
+     
     printf("\n");
 }
 
@@ -360,7 +418,7 @@ void recursicvePrint(char *basePath, Option *option) {
     struct dirent *dp;
     DIR *dir;
     struct stat buf;
-    char *path = (char*)malloc(1000);
+    char *path = (char*)malloc(PATH_MAX);
 
     if((dir = opendir(basePath)) == NULL) {
         perror ("Cannot open.");
@@ -383,18 +441,18 @@ void recursicvePrint(char *basePath, Option *option) {
         exit(EXIT_FAILURE);
     }
     // https://stackoverflow.com/questions/4989431/how-to-use-s-isreg-and-s-isdir-posix-macros
+ 
     for(int i =0; i < n; i++) {  
         dp = namelist[i];   
         if (dp->d_name[0] == '.') { 
             continue;     
         }
-        memset(path, 0, sizeof(path));
+        // memset(path, 0, sizeof(path));
         if (dp->d_name[0] == '.') { 
             continue;     
         }       
       
         strcpy(path, dp->d_name);
-        
         if(basePath != NULL) {
             strcpy(path, basePath);
             strcat(path, "/");
@@ -418,45 +476,97 @@ void recursicvePrint(char *basePath, Option *option) {
             recursicvePrint(path, option);
         }
     }
-    free(path);
+    
     closedir(dir);
+    free(path);
+    free(namelist);
+    free(dp);
+}
+
+int myStringCmp (char* str1, char* str2) {
+    char buf1[1000];
+    char buf2[1000];
+
+    strcpy(buf1, str1);
+    strcpy(buf2, str2);
+
+
+    int strLength1 = strlen(buf1);
+    int strLength2 = strlen(buf2);
+
+
+    int minStrLength;
+
+    //find the bounds for the loop
+    if(strLength1 <= strLength2) {
+        minStrLength = strLength1;
+    } else {
+        minStrLength = strLength2;
+    }
+
+    
+    // loop through and compare characters
+    for(int i = 0; i < minStrLength; i++) {
+        if(buf1[i] < buf2[i]) {
+            return -1;
+        } else if (buf1[i] > buf2[i]) {
+            return 1;
+        }
+    }
+    return strcmp(buf1, buf2);
 }
 
 
 
 int main (int argc, char *argv[]) {
     int count = 0;
-    char *path = (char*)malloc(1000);
-    char cwd[255]; 
+    char *path = (char*)malloc(PATH_MAX);
+    char cwd[PATH_MAX];
+    memset(cwd, 0, PATH_MAX); 
     Option *option = malloc(sizeof(Option));
     get_options(argc, argv, option); 
+   
+
+   char temp[1000];
+    int dif;
+    for (int i = 0; i< argc; i++) {
+        for(int j = i + 1; j< argc; j++) {
+            dif = myStringCmp(argv[i], argv[j]);
+            if(dif > 0) {
+                strcpy(temp, argv[i]);
+                argv[i] = argv[j];
+                argv[j] = temp;
+            }
+        }
+    }
+    // printf("Count the number of options: %d\n", count_opts);
+    // printf("THE NUMBER OF NON ARGUMENTS %d\n", non_opts);
 
     // loop through all args and skips the options[ -i, -l, -il etc...]
-    for (int i = 1; i < argc; i++)
+    for (int i = argc - non_opts; i < argc; i++)
     { 
-        printf("arguments: %s\n", argv[i]);
-
-
-        if (added_options(argv[i])) 
-            continue;
+        // printf("here are the arguments %d: , %s\n", i, argv[i]);
+       
+        // if (added_options(argv[i])) 
+        //     continue;
          
 
         if (!valid_directory(argv[i]) && !valid_file(argv[i])) {
             printf("ls: cannot access %s", argv[i]);
             printf("No such file or directory\n");
-            //exit(1);
+            exit(1);
         }
 
         // The number of args that are either a dir or a file
-        else if (directory(argv[i]) || single_file(argv[i])) {
+        else if (directory(argv[i]) || single_file(argv[i]) || valid_link(argv[i])) 
+        {
             if (valid_directory(argv[i]) || valid_file(argv[i])) 
             {
-                
                 count++;
             }
         }
     }
-    printf("count %d\n",count);
+    // printf("count %d\n",count);
     // https://pubs.opengroup.org/onlinepubs/009695399/functions/getcwd.html
     // no options given
     if (count == 0) {
@@ -468,31 +578,36 @@ int main (int argc, char *argv[]) {
         if(option->option_R) {
             recursicvePrint(".", option);
         } else {
+            
             print_directory(cwd, option);
         }    
     }
 
     // [ARGUEMENTS]
-    for (int j = 1; j < argc; j++) 
+    for (int j = argc - non_opts; j < argc; j++) 
     {   
+        
+
+
         // skips the option arguments ie. -i, -l etc..
         // only print directory and file name
-        if (added_options(argv[j])) 
-        {
-            continue; 
-        }
+        
+        // if (added_options(argv[j])) 
+        // {
+        //     continue; 
+        // }
              
         path = argv[j];
         if (single_file(path) || valid_link(path))
         {
-        
             print_file(path, option);
         } else if(directory(path)) {
-            if(option->option_R) {
+            if(option->option_R) 
+            {
                 recursicvePrint(path, option);
             } 
             else
-            {
+            {  
                 printf("%s:\n", path);
                 print_directory(path, option);
             }
@@ -520,6 +635,7 @@ int main (int argc, char *argv[]) {
     // }
     // free(path);
     free(option);
+    // free(path);
 
     return 0;
 }
